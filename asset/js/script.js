65ee3788d3e4812935b984c6e7d07d00cb9d0d93
DamarAureva <damaraurevadaviantara@gmai.com>
2024-08-28 11:10:57 +0700

    add fitur login

---
+++ asset/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const correctUsername= "Damar";
+    const correctPassword= "Damar123";
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === correctUsername && password === correctPassword){
+        alert('Login Berhasil');
+        window.location.href = 'index.html'
+    }else{
+        alert('Username atau Password salah');
+    }
+});
